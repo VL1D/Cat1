e6c2e76189202123eacb7be8b5e6033043ce8f1e
VL1D <123503817+VL1D@users.noreply.github.com>
2023-11-24 18:57:50 +0200

    level1

--- Cat1/Assets/skripts/level/LevelManager.cs
+++ Cat1/Assets/skripts/level/LevelManager.cs
@@ -6,10 +6,10 @@ public class LevelManager : MonoBehaviour
 {
    public static LevelManager instance;
 
-    public Transform respawnPointLevel1;
-    public GameObject Level1Prefab;
-    public Transform respawnPointLevel2;
-    public GameObject Level2Prefab;
+    public Transform[] respawnPointLevel;
+    public GameObject[] LevelPrefab;
+   // public Transform respawnPointLevel2;
+    //public GameObject Level2Prefab;
 
     public void Awake()
     {
@@ -18,11 +18,11 @@ public class LevelManager : MonoBehaviour
 
     public void RespawnLevel2()
     {
-        Instantiate(Level2Prefab, respawnPointLevel2.position , Quaternion.identity);
+        Instantiate(LevelPrefab[1], respawnPointLevel[1].position , Quaternion.identity);
     }
 
     public void RespawnLevel1()
     {
-        Instantiate(Level1Prefab, respawnPointLevel1.position, Quaternion.identity);
+        Instantiate(LevelPrefab[0], respawnPointLevel[0].position, Quaternion.identity);
     }
 }
