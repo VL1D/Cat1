0eb9654d74a555e7ec3881bf6e6becbcc31e0e9d
VL1D <123503817+VL1D@users.noreply.github.com>
2024-01-15 22:09:14 +0200

    Gattter

--- Cat1/Assets/skripts/Puzzle/Goal/Gates.cs
+++ Cat1/Assets/skripts/Puzzle/Goal/Gates.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gates : MonoBehaviour
 {
     public float speed;
-    public GameObject Gat;
+    public Transform Gat;
     public Animator animGat;
     public Transform poinsClose;
 
@@ -26,22 +27,36 @@ public class Gates : MonoBehaviour
     private void OpenGat()
     {
         transform.position = Vector2.MoveTowards(transform.position, poinsClose.transform.position, speed * Time.deltaTime);
-        animGat.SetBool("isOpen", true);
+        if (speed == 8)
+        {
+            animGat.SetBool("isOpen", true);
+            animGat.SetBool("isClose", false);
+        }
+        else if (speed == 0)
+        {
+            animGat.SetBool("isOpen", false);
+        }
         if (transform.position == poinsClose.transform.position)
         {
             speed = 0;
-            animGat.SetBool("isOpen", false);
         }
     }
 
     private void CloseGat()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Gat.transform.position, speed * Time.deltaTime );
-        animGat.SetBool("isClose", true);
+        transform.position = Vector2.MoveTowards(transform.position, Gat.transform.position, speed * Time.deltaTime);
+        if (speed == 3)
+        {
+            animGat.SetBool("isClose", true);
+            animGat.SetBool("isOpen", false);
+        }
+        else if(speed == 0)
+        {
+            animGat.SetBool("isClose", false);
+        }
         if(transform.position == Gat.transform.position)
         {
             speed = 0;
-            animGat.SetBool("isClose", false);
         }
     }
 }
