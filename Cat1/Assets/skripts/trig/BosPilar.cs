580cc7d377363a8535d7dbe9a08cd4f7f1e68b69
VL1D <123503817+VL1D@users.noreply.github.com>
2024-05-20 22:55:54 +0300

    Efect Star Boss

--- Cat1/Assets/skripts/trig/BosPilar.cs
+++ Cat1/Assets/skripts/trig/BosPilar.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class BosPilar : MonoBehaviour
 {
     public Rigidbody2D[] rbStone;
+    public GameObject Ef;
+    public GameObject Boos;
 
-
-
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             rbStone[0].isKinematic = false;
             rbStone[1].isKinematic = false;
+            Ef.SetActive(true);
+            StartCoroutine(ActBoos());
         }
+    }
+
+    private IEnumerator ActBoos()
+    {
+        yield return new WaitForSeconds(8f);
+        Boos.SetActive(true);
+
     }
 }
